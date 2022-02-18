24d53ab657e978f68fb8b909137ffd07b510d6a1
eno-conan <enokoh49@gmail.com>
2022-02-18 20:56:18 +0900

    save new user

--- ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/user/UserController.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
 
 @Controller
@@ -20,6 +23,24 @@ public class UserController {
 		List<User> users = service.listAll();
 		model.addAttribute("users", users);
 		return "users";
+	}
+
+	@GetMapping("/users/new")
+	public String newUser(Model model) {
+		User user = new User();
+		user.setEnabled(true);
+		List<Role> roles = service.listRoles();
+		model.addAttribute("roles", roles);
+		model.addAttribute("user", user);
+		return "user_form";
+	}
+
+	@PostMapping("/users/save")
+	public String saveUser(User user, RedirectAttributes redirectAttributes) {
+		System.out.println(user);
+		service.save(user);
+		redirectAttributes.addFlashAttribute("msg", "success create new user");
+		return "redirect:/users"; // usersのトップページにリダイレクト（再送信を防ぐ目的もあり）
 	}
 
 }
--- ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/user/UserService.java
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/user/UserService.java
@@ -5,16 +5,31 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.shopme.common.entity.Role;
 import com.shopme.common.entity.User;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository userRepo;
-	
-	public List<User> listAll(){
-		return (List<User>) userRepo.findAll(); //userRepo.findAll() : <Iterable> User -> Cast List User
+
+	@Autowired
+	private RoleRepository roleRepo;
+
+	public List<User> listAll() {
+		return (List<User>) userRepo.findAll(); // userRepo.findAll() : <Iterable> User -> Cast List User
+	}
+
+	public List<Role> listRoles() {
+		return (List<Role>) roleRepo.findAll();
+	}
+
+	public void save(User user) {
+		User savedInfo = userRepo.save(user);
+		if (savedInfo == null) {
+			System.out.println("not registered any user info");
+		}
 	}
 
 }
