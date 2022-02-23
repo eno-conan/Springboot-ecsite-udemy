105109bb06c76a7a4e4bff2f90fc70a1104b4b36
eno-conan <enokoh49@gmail.com>
2022-02-23 22:54:35 +0900

    use fragment and ready commonjs , AND... Fix some code caused by SpringSecurity's version

--- ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
+++ ShopmeWebParent/ShopmeBackend/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
@@ -2,9 +2,13 @@ package com.shopme.admin.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -13,13 +17,43 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Bean
+	public UserDetailsService userDetailsService() {
+		return new ShopmeUserDetailsService();
+	}
+
+	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
 
+	public DaoAuthenticationProvider daoAuthenticationProvider() {
+		DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+		authProvider.setUserDetailsService(userDetailsService());
+		authProvider.setPasswordEncoder(passwordEncoder());
+
+		return authProvider;
+
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.authenticationProvider(daoAuthenticationProvider());
+	}
+
+//	以下メールアドレスとパスワードでテストログイン可能
+//	test@gmail.com -> testtest
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests().anyRequest().permitAll();
+		http.authorizeRequests().anyRequest().authenticated().and().formLogin().loginPage("/login")
+				.usernameParameter("email").permitAll().and().logout().permitAll().and().rememberMe()
+				.key("Abcdefghijkl_123456789").tokenValiditySeconds(60 * 60 * 24 * 7);
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		// この設定を実装しないと、画像ファイルが読み込みできないようになっている:configure(HttpSecurity http)メソッドの実装により
+		// webjars:bootstrapの読み込みで必要→Spring　Securityの影響で全てのフォルダでやらないとエラー
+		web.ignoring().antMatchers("/images/**", "/js/**", "/fontawesome/**", "/css/**", "/webjars/**");
 	}
 
 }
---
+++ ShopmeWebParent/ShopmeBackend/src/main/resources/static/js/common.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+	$("#logoutLink").on("click", function(e) {
+		e.preventDefault();
+		document.logoutForm.submit();
+	});
+});
+
+
+function customizeDropDownMenu() {
+
+};
