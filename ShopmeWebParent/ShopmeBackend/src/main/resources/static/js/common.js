aeb175315ca5433443220491fb02dee41407aafd
eno-conan <enokoh49@gmail.com>
2022-02-24 21:20:56 +0900

    fixed menu-navbar and check password match in updating user

--- ShopmeWebParent/ShopmeBackend/src/main/resources/static/js/common.js
+++ ShopmeWebParent/ShopmeBackend/src/main/resources/static/js/common.js
@@ -9,6 +9,18 @@ $(document).ready(function() {
 
 
 function customizeDropDownMenu() {
+
+	$(".navbar .dropdown").hover(
+		/* mouse over */
+		function() {
+			$(this).find('.dropdown-menu').first().stop(true, true).delay(250).slideDown();
+		},
+		/* mouse out */
+		function() {
+			$(this).find('.dropdown-menu').first().stop(true, true).delay(100).slideUp();
+		}
+	);
+
 	$(".dropdown > a").click(function() {
 		location.href = this.href;
 	})
