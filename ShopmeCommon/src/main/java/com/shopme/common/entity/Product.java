bcbd37f870bc68263d3f41fdf79fccfcc388462e
eno-conan <enokoh49@gmail.com>
2022-03-02 21:03:54 +0900

    create Product Entity and run unit test

---
+++ ShopmeCommon/src/main/java/com/shopme/common/entity/Product.java
@@ -0,0 +1,74 @@
+package com.shopme.common.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "products")
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(unique = true, length = 256, nullable = false)
+	private String name;
+
+	@Column(unique = true, length = 256, nullable = false)
+	private String alias;
+
+	@Column(name = "short_description", length = 512, nullable = false)
+	private String shortDescription;
+
+	@Column(name = "full_description", length = 4096, nullable = false)
+	private String fullDescription;
+
+	@Column(name = "created_time")
+	private Date createdTime;
+
+	@Column(name = "updated_time")
+	private Date updatedTime;
+
+	private boolean enabled;
+
+	@Column(name = "in_stock")
+	private boolean inStock;
+
+	private float cost;
+	private float price;
+
+	@Column(name = "discount_percent")
+	private float discountPercent;
+
+	private float length;
+	private float width;
+	private float height;
+	private float weight;
+
+	@ManyToOne
+	@JoinColumn(name = "category_id")
+	private Category category;
+
+	@ManyToOne
+	@JoinColumn(name = "brand_id")
+	private Brand brand;
+
+	@Override
+	public String toString() {
+		return "Product [id=" + id + ", name=" + name + "]";
+	}
+
+}
---
+++ ShopmeWebParent/ShopmeBackend/src/test/java/com/shopme/admin/product/ProductRepositoryTests.java
@@ -0,0 +1,80 @@
+package com.shopme.admin.product;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Date;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.Rollback;
+
+import com.shopme.common.entity.Brand;
+import com.shopme.common.entity.Category;
+import com.shopme.common.entity.Product;
+
+@DataJpaTest(showSql = false) // falseにしておくと、結果が見やすい
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@Rollback(false)
+public class ProductRepositoryTests {
+
+	@Autowired
+	private ProductRepository repo;
+
+	@Autowired
+	private TestEntityManager entityManager;
+
+	@Test
+	public void testCreateProduct() {
+		Brand brand = entityManager.find(Brand.class, 2);
+		Category category = entityManager.find(Category.class, 10);
+
+		Product product = new Product();
+		product.setName("Test Product");
+		product.setAlias("Test Product alias");
+		product.setShortDescription("short description for test product");
+		product.setFullDescription("full description for test product");
+
+		product.setBrand(brand);
+		product.setCategory(category);
+
+		product.setPrice(1500);
+		product.setCost(200);
+		product.setCreatedTime(new Date());
+		product.setUpdatedTime(new Date());
+
+		Product savedProduct = repo.save(product);
+
+		assertThat(savedProduct).isNotNull();
+		assertThat(savedProduct.getId()).isGreaterThan(0);
+	}
+
+	@Test
+	public void testListAllProducts() {
+		Iterable<Product> iterableProducts = repo.findAll();
+		iterableProducts.forEach(p -> System.out.println(p));
+	}
+
+	@Test
+	public void testGetProduct() {
+		Product testProduct = repo.findById(1).get();
+
+		assertThat(testProduct.getId()).isEqualTo(1);
+		assertThat(testProduct.getName()).isEqualTo("Test Product");
+	}
+	
+	@Test
+	public void testUpdateProduct() {
+		Product testUpdateProduct = repo.findById(2).get();
+		testUpdateProduct.setName("Test Product2 Updated");
+		
+		Product savedProduct = repo.save(testUpdateProduct);
+
+		assertThat(savedProduct.getId()).isEqualTo(2);
+		assertThat(savedProduct.getName()).isEqualTo("Test Product2 Updated");
+	}
+
+}
